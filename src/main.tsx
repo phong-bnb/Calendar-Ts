77e5eeda29e6752b5dc2234ead102ce238728ceb
Phong NG <ngphong.7800@gmail.com>
2024-10-09 14:14:39 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux"; // Thêm import này
 import store from "./store.ts"; // Đường dẫn đến file store
 import App from "./App"; // Đường dẫn đến file App
-
+import "./index.css";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
